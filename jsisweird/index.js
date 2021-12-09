df96b4d7581118bd604243afa67c6252281e1156
giulianoojeda777 <giuliano.ojeda@gmail.com>
2021-12-09 14:24:25 -0300

    tests

---
+++ jsisweird/index.js
@@ -0,0 +1,25 @@
+console.log(true + false); 
+console.log([,,,].length);
+console.log([1, 2, 3] + [4, 5, 6]);
+console.log(0.2 + 0.1 === 0.3);
+console.log((10,2));
+console.log(!!"");
+console.log(+!![]);
+console.log(!!!true);
+console.log(true == "true");
+console.log(010 - 03);
+console.log("" - - "");
+console.log(null + 0);
+console.log(0/0);
+console.log(1/0 > 10 ** 1000);
+//console.log(true++);
+console.log("" - 1);
+console.log((null - 0) + "0");
+console.log(true + ("true" - 0));
+console.log(!5 + !5);
+console.log([] + []);
+console.log(1 + 2 + "3");
+console.log(typeof NaN);
+console.log(undefined + false)
+console.log("" && -0)
+console.log(+!!NaN * "" - - [,])
