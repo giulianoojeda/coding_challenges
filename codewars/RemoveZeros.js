e302c72f525ddca7591e94e1923963e316c47b33
giulianoojeda777 <giuliano.ojeda@gmail.com>
2021-12-10 16:54:06 -0300

    today's practice

---
+++ codewars/RemoveZeros.js
@@ -0,0 +1,35 @@
+/*
+
+Write a function that takes an array of values and moves all elements that are zero to the end of the array, otherwise preserving the order of the array. The zero elements must also maintain the order in which they occurred.
+
+For example, the following array
+
+[7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]
+
+is transformed into
+
+[7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0]
+
+Zero elements are defined by either 0 or "0". Some tests may include elements that are not number literals.
+
+You are NOT allowed to use any temporary arrays or objects. You are also not allowed to use any Array.prototype or Object.prototype methods.
+
+*/
+
+function removeZeros(array) {
+  let limit = array.length;
+  let cache;
+  for (let i = 0; i < limit; i++) {
+    if (array[i] === 0 || array[i] === "0") {
+      cache = array[i];
+      for (let j = i--; j < array.length - 1; j++) {
+        array[j] = array[j + 1];
+      }
+      array[array.length - 1] = cache;
+      limit--;
+    }
+  }
+  return array;
+}
+
+console.log(removeZeros([7, 2, 3, 0, 4, 6, 0, "0", 13, 0, 78, 0, 0, 19, 14]));
